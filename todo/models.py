abd1abda125ecd823e731ac4f3adf765d8e762eb
Wasth <riasta@yandex.ru>
2020-01-09 14:54:35 +0300

    Little models schema

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+	name = models.CharField(max_length=250)
+
+	def __str__(self):
+		return 'List: ' + self.name
+
+
+class Task(models.Model):
+	text = models.CharField(max_length=500)
+	list = models.ForeignKey(
+		List, related_name='tasks', on_delete=models.CASCADE
+	)
+
+	def __str__(self):
+		return 'Task: ' + self.text
